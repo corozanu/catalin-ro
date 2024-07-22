3c225d306d1ecf017bd38241b05a67d4467d7ad5
Cătălin Corozanu <Catalin@Corozanu.ro>
2024-07-22 16:35:30 +0300

    ctl-logo

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://transitivebullsh.it/page-icon.png',
+  defaultPageIcon: 'https://cdn.crz.ro/catalin.ro/images/ctl-logo150x150.png',
   defaultPageCover: 'https://transitivebullsh.it/page-cover.jpg',
   defaultPageCoverPosition: 0.5,
 
