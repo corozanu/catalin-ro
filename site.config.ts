1e5ee0271e15b9fb077ee4c989f75c72f667229b
Cătălin Corozanu <Catalin@Corozanu.ro>
2024-07-22 16:19:09 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -20,6 +20,7 @@ export default siteConfig({
   twitter: 'corozanu',
   github: 'corozanu',
   linkedin: 'corozanu',
+  facebook: 'catalin.corozanu',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
